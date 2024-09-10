04273f442738d938b45422598435b0ad534be10b
Medha Moorching <mepiper2010@gmail.com>
2024-09-10 12:50:39 +0530

    removing proxy in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const backendUrl = process.env.VITE_BACKEND_URL || 'http://localhost:5000';
+const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Proxy API requests starting with /api to the backend server
-      "/api": {
-        target: backendUrl, // Replace with your backend's local development URL
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ""), // Optionally remove the /api prefix
-      },
-    },
-  },
 });
