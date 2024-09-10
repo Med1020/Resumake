d918ddc5cdad3da7b6d594451c269875a70f6a4e
Medha Moorching <mepiper2010@gmail.com>
2024-09-10 12:43:33 +0530

    adding proxy in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const backendUrl = process.env.VITE_BACKEND_URL || 'http://localhost:5000';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,7 +9,7 @@ export default defineConfig({
     proxy: {
       // Proxy API requests starting with /api to the backend server
       "/api": {
-        target: import.meta.env.VITE_BACKEND_URL, // Replace with your backend's local development URL
+        target: backendUrl, // Replace with your backend's local development URL
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ""), // Optionally remove the /api prefix
       },
