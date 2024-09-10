e8cf862111a2a592bd7bec096f0b459e46e0be5d
Medha Moorching <mepiper2010@gmail.com>
2024-09-10 12:54:56 +0530

    removing proxy in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
